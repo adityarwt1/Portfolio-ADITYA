9f3d46bb239da55e795442e54f884c4a78ca1e38
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-19 21:38:54 +0530

    some basic api written

--- app/api/v1/getSkills/route.ts
+++ app/api/v1/getSkills/route.ts
@@ -1,8 +1,13 @@
+import { mongoconnect } from "@/lib/mongodb";
 import {  NextResponse } from "next/server";
 
 export async function GET(){
     try {
-        
+        const isConnected = await mongoconnect()
+
+        if(!isConnected){
+            return NextResponse.json({error:"Internal server issue"}, {status:500})
+        }
     } catch (error) {
         console.log(error)
         return NextResponse.json({error:"Internal server issue."},{status:500})
