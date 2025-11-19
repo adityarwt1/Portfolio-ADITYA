d6e92e065b66bec2f4a15b7c328fe7c6ab4aa1ec
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-19 23:04:52 +0530

    saved to cookie

---
+++ app/api/v1/login/route.ts
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken'
+import { cookies } from "next/headers";
+export async function  POST(req:NextRequest) {
+
+    try {
+        const {email, password} = await req.json()
+
+        if(!email || !password){
+            return NextResponse.json({error:"email or password must be required."},{status:400})
+        }
+        const admingEmail = "adityarawatnew2487@gmail.com";
+        const hashedPassword =
+          "$2b$10$7/wv7CLM60TikzXkbudBVOgn53NiR0kin3cUZkmHx3qdr3/k.yczG";
+
+        if(email !== admingEmail){
+            return NextResponse.json({error:"You are not an admin."},{status:400})
+        }
+
+        const isPassswordTrue = await bcrypt.compare(password , hashedPassword)
+
+        if(!isPassswordTrue){
+            return NextResponse.json({error:"Wrong password."},{status:400})
+        }
+
+        const tokenPayLoad = {
+            role: "admin"
+        }
+        const token = jwt.sign(tokenPayLoad , process.env.JWT_SECRET as string,{
+            expiresIn: 30 *  60 * 60  * 1000,
+        });
+
+        (await cookies()).set("portfoliotoken" , token)
+        return NextResponse.json({success:true},{status:200})
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({error:"Internal server issue."},{status:500})
+    }
+    
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,12 @@
 "use client"
 import React, { useState } from "react";
-import CryptoJs from 'crypto-js'
 import { useRouter } from "next/navigation";
 
 const LogiPage:React.FC = ()=>{
     // state declaration
     const [email, setEmail] = useState<string>()
     const [password, setPassword] = useState<string | undefined>()
-    const [error, setError] = useState<string>("adfasdf")
+    const [error, setError] = useState<string>()
     const [loading, setLoading] = useState<boolean>(false)
     const [isHid, setIsHide]  = useState<boolean>(true)
     const router = useRouter()
@@ -15,18 +14,19 @@ const LogiPage:React.FC = ()=>{
     //handle submit button
     const handleSubmit = async (e:React.FormEvent)=>{
         e.preventDefault()
-
+        
         if(!password || !email || typeof(password) == "undefined"){
             setError("Email or password must be required.")
+            return
         }
+        setLoading(true)
         try {
-            const encryptedPassword =  CryptoJs.AES.encrypt(password as string, process.env.CRYPTO_SECRET as string)
             const response = await fetch("/api/v1/login",{
                 method:"POST",
                 headers:{
                     "Content-Type":"application/json"
                 },
-                body:JSON.stringify({email, password: encryptedPassword})
+                body:JSON.stringify({email, password})
             })
             const data = await response.json()
 
@@ -39,6 +39,9 @@ const LogiPage:React.FC = ()=>{
         } catch (error) {
             setError((error as Error).message)
         }
+        finally{
+            setLoading(false)
+        }
     }
 
     // handle hide and show password
@@ -50,19 +53,39 @@ const LogiPage:React.FC = ()=>{
         }
     }
     return (
-        <div className="flex w-full h-screen justify-center items-center">
-            <div className="p-10 border flex flex-col ">
-                <div>ADMIN LOGIN</div>
-                <form onSubmit={handleSubmit} className="flex  flex-col">
-                    <input type="email" onChange={(e) => setEmail(e.target.value)} className="border"/>
-                    <input type={isHid ? "password" :"text"} onChange={(e) => setPassword(e.target.value)} className="border"/>
-                    <button type="submit">Submit</button>
-                </form>
-                {error ? (
-                    <div>{error}</div>
-                ):""}
+      <div className="flex w-full h-screen justify-center items-center">
+        <div className="p-10 border flex flex-col rounded-lg shadow-white shadow-2xs border-white/15">
+          <div className="text-6xl font-bold">ADMIN LOGIN</div>
+          <form
+            onSubmit={handleSubmit}
+            className="flex  flex-col justify-center items-center"
+          >
+            <input
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              className="border p-2 rounded-sm shadow-2xs shadow-white px-4 my-2 w-full"
+              placeholder="email..."
+            />
+            <div className="w-full flex h-fit ">
+              <input
+                type={isHid ? "password" : "text"}
+                onChange={(e) => setPassword(e.target.value)}
+                className="border-b border-t border-l p-2 sm-full shadow-2xs shadow-white focus:outline-none w-[90%]" 
+                placeholder="password..."
+              />
+              <button type="button"  onClick={handleHidePassword} className="w-[10%] border-t border-b border-r shadow-2xs shadow-white pr-2">{isHid ? "Show" :"Hide"}</button>
             </div>
+            <button
+              type="submit"
+              className="bg-white text-black w-full px-20 py-1 text-xl my-2 hover:opacity-80 cursor-pointer shadow-2xs shadow-white *:"
+              disabled={loading}
+            >
+             {loading ?"Submit...":"Submit"}
+            </button>
+          </form>
+          {error ? <div className="bg-red-500/90 text-shadow-2xs text-shadow-black p-4 rounded-md">{error}</div> : ""}
         </div>
-    )
+      </div>
+    );
 }
 export default LogiPage
