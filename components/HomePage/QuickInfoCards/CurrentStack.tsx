17e0feb22d7466c69897c1d1002e896dc5827089
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-21 14:46:52 +0530

    doen the current stack thing

--- components/HomePage/QuickInfoCards/CurrentStack.tsx
+++ components/HomePage/QuickInfoCards/CurrentStack.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CurrentStack = () => {
   return (
     <div className="border w-fit py-1 px-3 rounded-full bg-[#e0e0e0]/95 text-black font-mono">
-      Stack: MernStack
+      Stack:MernStack
     </div>
   )
 }
