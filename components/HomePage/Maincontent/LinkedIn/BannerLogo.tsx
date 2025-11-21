2ff080736b5fdf2a28b377dfb112c85f1b2e75d8
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-21 19:21:48 +0530

    done the bannerimage

--- components/HomePage/Maincontent/LinkedIn/BannerLogo.tsx
+++ components/HomePage/Maincontent/LinkedIn/BannerLogo.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const BannerLogo = () => {
   return (
-    <div className=''>
-      asdfasdfsdf
-    </div>
-  )
-}
+    <div className="flex flex-col bg-white">
+      <div className="relative w-full">
+        {/* Banner */}
+        <Image
+          src="/images/tripxbanner.jpg"
+          width={400}
+          height={200}
+          alt="Banner"
+          className="w-full h-auto object-cover"
+        />
 
-export default BannerLogo
+        {/* Left-aligned Perfect Round DP */}
+        <Image
+          src="/images/profilephoto.jpg"
+          width={80}
+          height={80}
+          alt="Profile Photo"
+          className="
+            absolute
+            left-5
+            -bottom-10
+            w-20
+            h-20
+            rounded-full
+            object-cover
+            border-4
+            border-white
+            shadow-lg
+          "
+        />
+      </div>
+
+      <div className="h-12"></div>
+      <div>adsfasdfasdf</div>
+    </div>
+  );
+};
+
+export default BannerLogo;
