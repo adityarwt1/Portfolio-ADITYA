c1c7dda4e729903e74e8dd084b1866a892db2cd0
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-21 14:46:06 +0530

    Leetcode question fixed

--- components/HomePage/QuickInfoCards/Leetcode.tsx
+++ components/HomePage/QuickInfoCards/Leetcode.tsx
@@ -9,7 +9,7 @@ const Leetcode:React.FC<LeetProblems>  = ({easy, medium, hard}) => {
   return (
     
       <div className="border w-fit py-1 px-3 rounded-full bg-[#e0e0e0]/95 text-black font-mono">
-        LeetCode: Easy {easy}, Medium {medium} Hard {hard}
+        LeetCode: Easy:{easy}, Medium:{medium} Hard:{hard}
       </div>
     
   )
