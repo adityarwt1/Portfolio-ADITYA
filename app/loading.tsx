4562eff94c71574324e9b8ed8cf3e44b08e6f334
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-23 13:00:21 +0530

    time to shift dark as well

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,10 @@
+import { Spinner } from '@/components/ui/spinner'
 import React from 'react'
 
 const HomePageLoading = () => {
   return (
-    <div className=''>
-      Loding...
+    <div className='w-full h-screen justify-center items-center '>
+      <Spinner />
     </div>
   )
 }
