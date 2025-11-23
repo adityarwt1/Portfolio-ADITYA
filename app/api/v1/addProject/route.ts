d40693ec0074d344e156180fcb42e433d8884ab8
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-23 18:59:29 +0530

    project is done

--- app/api/v1/addProject/route.ts
+++ app/api/v1/addProject/route.ts
@@ -5,9 +5,9 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req:NextRequest) {
 
     try {
-        const {title, description, skills , thumbNail} = await req.json()
+        const {title, description, skills ,keyFeature,  thumbNail , liveLink, gitHubLink , currentActive } = await req.json()
 
-        if(!title || !description || !skills || !thumbNail){
+        if(!title || !description || !skills || !thumbNail || !keyFeature || !liveLink || !gitHubLink){
             return   NextResponse.json({error:"fields are must be passed properly!"},{status: 400})
         }
 
@@ -18,9 +18,9 @@ export async function POST(req:NextRequest) {
             return NextResponse.json({error:"Failed to connect databse"},{status:500})
         }
 
-        await Project.insertOne({title, description, skills , thumbNail})
+    const project  =  await Project.create({title, description, skills , thumbNail , gitHubLink , keyFeature, liveLink, currentActive })
 
-        return NextResponse.json({success:true, message:"Skilled added successfull!"},{status:201})
+        return NextResponse.json({success:true, message:"Skilled added successfull!" , project},{status:201})
     } catch (error) {
         console.log(error)
         return  NextResponse.json({error:"Internal server issue."},{status:500})
--- models/Projects.ts
+++ models/Projects.ts
@@ -1,10 +1,16 @@
 import mongoose, { Document, Schema } from "mongoose";
 
 export interface ProjectInterface extends Document {
+    _id:mongoose.Types.ObjectId
+    __v:number
     title:string
     description:string
     skills:string[],
-    thumbNail:string
+    thumbNail:string,
+    keyFeature:string[]
+    liveLink:string
+    gitHubLink:string
+    active:boolean
 }
 
 const ProjectSchema :Schema<ProjectInterface> = new Schema({
@@ -21,6 +27,18 @@ const ProjectSchema :Schema<ProjectInterface> = new Schema({
     }],
     thumbNail:{
         type:String
+    },
+    keyFeature:[{
+        type:String
+    }],
+    liveLink:{
+        type:String
+    },
+    gitHubLink:{
+        type:String
+    },
+    active:{
+        type:Boolean
     }
 },{timestamps:true})
 
