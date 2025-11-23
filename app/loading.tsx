5f41bc69594471479b092f0633d0a7ee06b44734
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-23 15:06:35 +0530

    time to other pages

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,12 +1,55 @@
-import { Spinner } from '@/components/ui/spinner'
-import React from 'react'
+import React from "react";
 
-const HomePageLoading = () => {
+const SkeletonBadge = () => (
+  <div className="h-8 w-40 rounded-xl bg-zinc-200 dark:bg-zinc-700 animate-pulse"></div>
+);
+
+const HomeSkeleton = () => {
   return (
-    <div className='w-full h-screen justify-center items-center '>
-      <Spinner />
-    </div>
-  )
-}
+    <div
+      className="
+      w-full min-h-screen p-5 flex flex-col gap-4 
+      bg-white dark:bg-black
+      bg-[radial-gradient(circle,rgba(0,0,0,0.15)_1px,transparent_1px)]
+      dark:bg-[radial-gradient(circle,rgba(255,255,255,0.15)_1px,transparent_1px)]
+      bg-size-[20px_20px]
+      animate-pulse
+    "
+    >
+      {/* TOP BADGES */}
+      <div className="flex flex-wrap justify-center gap-1">
+        <SkeletonBadge />
+        <SkeletonBadge />
+        <SkeletonBadge />
+        <SkeletonBadge />
+        <SkeletonBadge />
+        <SkeletonBadge />
+      </div>
 
-export default HomePageLoading
+      {/* MAIN HERO SKELETON */}
+      <div className="flex-1 flex justify-center items-center">
+        <div className="flex flex-col items-center gap-4 w-full max-w-3xl px-10">
+          {/* Name */}
+          <div className="h-10 w-72 bg-zinc-200 dark:bg-zinc-700 rounded-xl"></div>
+
+          {/* Paragraph lines */}
+          <div className="w-full flex flex-col gap-2 items-center">
+            <div className="h-4 w-[90%] bg-zinc-200 dark:bg-zinc-700 rounded-xl"></div>
+            <div className="h-4 w-[85%] bg-zinc-200 dark:bg-zinc-700 rounded-xl"></div>
+            <div className="h-4 w-[80%] bg-zinc-200 dark:bg-zinc-700 rounded-xl"></div>
+            <div className="h-4 w-[70%] bg-zinc-200 dark:bg-zinc-700 rounded-xl"></div>
+            <div className="h-4 w-[60%] bg-zinc-200 dark:bg-zinc-700 rounded-xl"></div>
+          </div>
+
+          {/* Buttons */}
+          <div className="flex gap-3">
+            <div className="h-10 w-32 bg-zinc-200 dark:bg-zinc-700 rounded-xl"></div>
+            <div className="h-10 w-32 bg-zinc-200 dark:bg-zinc-700 rounded-xl"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSkeleton;
